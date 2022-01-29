38e02abe1097fa3964ba9152818fb0c91c3e8d37
Imamul Kadir <imamulkadiroyan@gmail.com>
2022-01-29 13:54:47 +0600

    initial Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Like Count*/
+
+function incrementButton(){
+    var element = document.getElementById('incrementText');
+    var value = element.innerHTML;
+
+    ++value;
+    document.getElementById('incrementText').innerHTML = value;
+}
+
+/*Quote Generator*/
+
+const text = document.querySelector('.quotes');
+const author = document.querySelector('.author');
+const nextBtn = document.querySelector('.next');
+
+const getQuote = async() => {
+    const res = await fetch('https://type.fit/api/quotes');
+    const quotes = await res.json();
+    const num = Math.floor(Math.random()*quotes.length);
+    const item = quotes[num];
+    const quote = item.text;
+    const authorName = item.author;
+    text.innerText = quote;
+    author.innerText = authorName;
+    // console.log(item); 
+}
+
+nextBtn.addEventListener('click', getQuote);
+
+getQuote()
